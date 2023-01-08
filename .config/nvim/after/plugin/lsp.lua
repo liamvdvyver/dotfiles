c575aca9cb73b95e9e3ae66d6676a6872b336f6d
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2023-01-08 17:30:52 +1100

    feat!: nvim switch to native lsp

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,16 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'sumneko_lua',
+  'pyright',
+  'bashls',
+  'texlab',
+  'r_language_server',
+})
+
+lsp.setup()
+
+-- Configure lua language server for neovim
+lsp.nvim_workspace()
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -62,24 +62,26 @@ return require('packer').startup(function(use)
   -- language/syntax
 
   use {
-    'neoclide/coc.nvim', branch = 'release',
-    opt = true, ft = {'python', 'tex', 'bib', 'r', 'rmd', 'sh'},
-    run = ':CocInstall',
-    config = function()
-      if packer_plugins["coc.nvim"] and packer_plugins["coc.nvim"].loaded then
-        vim.cmd[[
-          source ~/.config/nvim/coc.vim
-          " install languageserver R package for coc-r-lsp
-          " install shellcheck and shfmt system packages for coc-diagnostic
-          let g:coc_global_extensions = [
-            \ 'coc-pyright',
-            \ 'coc-vimtex',
-            \ 'coc-r-lsp',
-            \ 'coc-diagnostic',
-            \ ]
-        ]]
-      end
-    end
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      -- Snippet Collection (Optional)
+      {'rafamadriz/friendly-snippets'},
+    }
   }
 
   use {
