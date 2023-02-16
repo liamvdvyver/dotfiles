98de6c8adbef5db309739c1956002138c8b99428
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2023-02-17 00:07:09 +1100

    chore: nvim specify lualine theme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -178,6 +178,7 @@ return require('packer').startup(function(use)
     config = function()
       require('lualine').setup {
         options = {
+          theme = 'gruvbox',
           sections = { lualine_a = {'mode', 'branch'} },
           section_separators = '',
           component_separators = ''
