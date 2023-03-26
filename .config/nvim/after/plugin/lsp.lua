c8f5f454f19f73ceb85ca7ed91d926dcb5e66b46
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2023-03-27 08:36:49 +1100

    chore: nvim lsp-zero manyally configure cmp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,6 +1,13 @@
 local lsp = require('lsp-zero')
 
-lsp.preset('recommended')
+lsp.preset({
+  name  = 'recommended',
+  suggest_lsp_servers = false,
+  set_lsp_keymaps = {
+    omit = {'<C-k>'},
+    preserve_mappings = false
+  },
+})
 
 lsp.ensure_installed({
   'lua_ls',
@@ -9,6 +16,21 @@ lsp.ensure_installed({
   'texlab',
   'r_language_server',
   'marksman',
+})
+
+lsp.setup_nvim_cmp({
+  sources = {
+    {name = 'luasnip'},
+    {name = 'nvim_lsp'},
+    {name = 'path'},
+    -- {name = 'omni'},
+    -- {name = 'cmp_nvim_r'},
+    -- {name = 'nvim_lua'},
+  },
+  preselect = 'none',
+  completion = {
+    completeopt = 'menu,menuone,noinsert,noselect'
+  },
 })
 
 lsp.setup()
