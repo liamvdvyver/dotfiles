1d3fe9cc5c72ced614078fcdb0cedf25b1825cff
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-09-05 18:08:09 +1000

    improvement: lazy-load snippets

---
+++ .config/nvim/lua/plg/plugins/friendly-snippets.lua
@@ -0,0 +1,8 @@
+return {
+  'rafamadriz/friendly-snippets',
+  dependencies = { 'L3MON4D3/LuaSnip' },
+  lazy = true, event = "VeryLazy",
+  config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+  end
+}
