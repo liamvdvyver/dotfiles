05625ddbc2621563703e3f69e96dd9f434abb1b3
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-09-25 10:35:23 +1000

    feat: add lsp info and venv to lualine

--- .config/nvim/lua/plg/plugins/lualine.lua
+++ .config/nvim/lua/plg/plugins/lualine.lua
@@ -1,12 +1,26 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = {'kyazdani42/nvim-web-devicons', name = 'nvim-web-devicons-lualine'},
+  dependencies = {
+    { 'kyazdani42/nvim-web-devicons', name = 'nvim-web-devicons-lualine' },
+    { 'WhoIsSethDaniel/lualine-lsp-progress.nvim' },
+  },
   opts = {
     options = {
       theme = 'gruvbox',
-      sections = {lualine_a = {'mode', 'branch'}},
       section_separators = '',
       component_separators = ''
-    }
+    },
+    sections = {
+      lualine_b = {
+        "branch", "diff",
+        { "diagnostics", sources = { "nvim_lsp" } }
+      },
+      lualine_c = {
+        'filename', 'lsp_progress'
+      },
+      lualine_x = {
+        'encoding', 'fileformat', 'filetype',
+        { "swenv", icon = "v", color = { fg = "" } } }
+    },
   }
 }
