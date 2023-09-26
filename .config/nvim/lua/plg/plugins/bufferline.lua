982101eedb92c8359784e4968b4c882f1766f0be
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-09-26 14:19:50 +1000

    improvement: nvim bufferline remove close icon

--- .config/nvim/lua/plg/plugins/bufferline.lua
+++ .config/nvim/lua/plg/plugins/bufferline.lua
@@ -1,8 +1,14 @@
 return {
   'akinsho/bufferline.nvim',
-  dependencies = {'nvim-tree/nvim-web-devicons', name = 'tree-nvim-web-devicons'},
-  config = function()
-    require("bufferline").setup()
+  dependencies = { 'nvim-tree/nvim-web-devicons', name = 'tree-nvim-web-devicons' },
+  opts = {
+    options = {
+      buffer_close_icon = "",
+      close_icon = "",
+    }
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
     vim.keymap.set("n", "<M-h>", ":BufferLineCyclePrev<CR>")
     vim.keymap.set("n", "<M-l>", ":BufferLineCycleNext<CR>")
     vim.keymap.set("n", "<M-,>", ":BufferLineMovePrev<CR>")
