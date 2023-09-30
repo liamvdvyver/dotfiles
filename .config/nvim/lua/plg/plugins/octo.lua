f1200c1443369050ac553835f242f96d1891c330
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-09-30 11:46:23 +1000

    feat: nvim add octo

---
+++ .config/nvim/lua/plg/plugins/octo.lua
@@ -0,0 +1,11 @@
+return {
+  "pwntester/octo.nvim",
+  requires = {
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons", name = "tree-nvim-web-devicons" },
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+  opts = {}
+}
