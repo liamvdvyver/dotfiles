b75dfbb2f37f2f318fd8de01f8cea344827a2746
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2024-01-02 14:48:58 +1100

    chore: nvim indent-blankline lua calls for binds

--- .config/nvim/lua/plg/plugins/indent-blankline.lua
+++ .config/nvim/lua/plg/plugins/indent-blankline.lua
@@ -3,9 +3,9 @@ return {
   opts = {},
   config = function(_, opts)
     require("ibl").setup(opts)
-    vim.keymap.set("n", "zc", "zc:IndentBlanklineRefresh<CR>")
-    vim.keymap.set("n", "zo", "zo:IndentBlanklineRefresh<CR>")
-    vim.keymap.set("n", "zM", "zM:IndentBlanklineRefresh<CR>")
-    vim.keymap.set("n", "zR", "zR:IndentBlanklineRefresh<CR>")
+    vim.keymap.set("n", "zc", "zc:lua require'ibl'.debounced_refresh(0)<CR>")
+    vim.keymap.set("n", "zo", "zo:lua require'ibl'.debounced_refresh(0)<CR>")
+    vim.keymap.set("n", "zM", "zM:lua require'ibl'.debounced_refresh(0)<CR>")
+    vim.keymap.set("n", "zR", "zR:lua require'ibl'.debounced_refresh(0)<CR>")
   end,
 }
