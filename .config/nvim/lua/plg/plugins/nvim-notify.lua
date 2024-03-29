f8c1fd1a3efd86971b3eaf6378578b7a8b4bbfac
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2024-03-29 17:14:22 +1100

    feat: new nvim keybinds

--- .config/nvim/lua/plg/plugins/nvim-notify.lua
+++ .config/nvim/lua/plg/plugins/nvim-notify.lua
@@ -1,6 +1,19 @@
 return {
   "rcarriga/nvim-notify",
+  lazy = false,
   config = function()
     vim.notify = require("notify")
   end,
+  keys = {
+    {
+      "<leader>j",
+      function()
+        vim.notify.dismiss({
+          pending = true,
+          silent = true,
+        })
+        vim.cmd([[noh]])
+      end,
+    },
+  },
 }
