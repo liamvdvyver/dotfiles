9335b704fa233467b423fd93726c100958085727
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2024-04-11 09:41:35 +1000

    feat: upgrade to harpoon2

--- .config/nvim/lua/plg/plugins/harpoon.lua
+++ .config/nvim/lua/plg/plugins/harpoon.lua
@@ -1,20 +1,27 @@
+
 return {
   "ThePrimeagen/harpoon",
-  config = function()
+  branch = "harpoon2",
+  requires = { "nvim-lua/plenary.nvim" },
+  opts = {},
+  config = function(_, opts)
+    local harpoon = require("harpoon")
+    harpoon:setup(opts)
+
     vim.keymap.set("n", "<Leader>m", function()
-      require("harpoon.ui").toggle_quick_menu()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "View project [m]arks" })
     vim.keymap.set("n", "<Leader>M", function()
-      require("harpoon.mark").add_file()
+      harpoon:list():add()
     end, { desc = "Add project [M]arks" })
     vim.keymap.set("n", "gh", function()
-      require("harpoon.ui").nav_file(1)
+      harpoon:list():select(1)
     end, { desc = "Open primary file" })
     vim.keymap.set("n", "gj", function()
-      require("harpoon.ui").nav_file(2)
+      harpoon:list():select(2)
     end, { desc = "Open secondary file" })
     vim.keymap.set("n", "gk", function()
-      require("harpoon.ui").nav_file(3)
+      harpoon:list():select(3)
     end, { desc = "Open tertiary file" })
   end,
 }
